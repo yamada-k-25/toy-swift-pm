14dc4d1853889d9c4f3654012e3736ba20aa1b7f
yamada-k-25 <yamada.kazuki.sg@alumni.tsukuba.ac.jp>
2024-06-15 01:10:37 +0900

    🎉 first commit

---
+++ toy-swift-pm/toy-swift-pm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toy-swift-pm
+//
+//  Created by yamada-k-25 on 2024/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toy-swift-pm/toy-swift-pm/toy_swift_pmApp.swift
@@ -0,0 +1,17 @@
+//
+//  toy_swift_pmApp.swift
+//  toy-swift-pm
+//
+//  Created by yamada-k-25 on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct toy_swift_pmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
